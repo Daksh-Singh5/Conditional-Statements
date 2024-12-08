969bef0088bfcb9eece97f921110d7ae53301df3
Daksh-Singh5 <dakshsingh6612@gmail.com>
2024-12-08 17:08:51 +0530

    hello

---
+++ project.py
@@ -0,0 +1,10 @@
+season= input("Which season is going on right now ")
+if season == "summer":
+    print("Were a cotton shirt and shorts")
+elif season == "winter":
+    print("Were fullsleve woolen shirt and pants")
+elif season == "spring":
+    print("Summer is coming, were a cotton shirt and shorts")
+elif season == "autumn":
+    print("Winter is coming, were fullsleve woolen shirt and pants")
+
